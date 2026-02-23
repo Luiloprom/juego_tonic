a041b2c3aa696bcf5bab77dedbbf6925b4c47ee6
Luiloprom <luis.lopez56@educa.madrid.org>
2026-02-23 01:52:03 +0100

    Moneda terminada

---
+++ coleccionables/moneda/moneda.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+
+func _ready():
+	$ani_moneda.play("default")
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("jugadores"):
+		body.add_moneda()
+		$audio_moneda.play()
+		queue_free()
